78d7e7050063f8df8a268c171efe3e161a40af01
Nikan Dalvand <nikandalvand@gmail.com>
2019-08-12 23:45:39 +0430

    Added counter reducer action and type

--- src/Data/Actions/CounterActions.jsx
+++ src/Data/Actions/CounterActions.jsx
@@ -1,2 +1,8 @@
 import * as types from './types'
 
+export const increaseBy = (number) => (dispatch) => {
+    dispatch({
+        type:types.INCREASE_BY,
+        payload: number
+    })
+}
--- src/Data/Reducers/CounterReducer.jsx
+++ src/Data/Reducers/CounterReducer.jsx
@@ -1,11 +1,16 @@
 import * as types from '../Actions/types'
 
 const initialState = {
-
+    counter: 0
 }
 
 export default function ModuleReducer(state = initialState,action) {
     switch(action.type) {
+        case types.INCREASE_BY:
+            return {
+                ...state,
+                counter: state.counter + action.payload
+            }
         default:
             return state;
     }
