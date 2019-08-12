bf868710483d19921722db256ce2fb2bc84ec736
Nikan Dalvand <nikandalvand@gmail.com>
2019-08-12 23:41:34 +0430

    Added default application reducer

--- src/Data/Store.jsx
+++ src/Data/Store.jsx
@@ -1,7 +1,9 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk'
+import ApplicationReducer from './Reducers/ApplicationReducer'
 
 const defaultReducers = {
+    ApplicationReducer
 }
 
 const initialState = {}
